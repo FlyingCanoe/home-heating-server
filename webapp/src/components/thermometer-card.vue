4f2068edc587227ec9bc7468079d739549098626
FlyingCanoe <FlyingCanoe@protonmail.com>
2022-07-07 17:01:49 -0400

    remove default plot data

--- webapp/src/components/thermometer-card.vue
+++ webapp/src/components/thermometer-card.vue
@@ -70,8 +70,8 @@ export default defineComponent({
 
   mounted() {
     let data = {
-      x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
-      y: [1, 3, 6],
+      x: [],
+      y: [],
     };
 
     let graph_div = this.$refs.graph as HTMLDivElement;
